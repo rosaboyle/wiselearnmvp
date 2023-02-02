706af6af4df1047fa77d7df91ce40bcffb14f1a5
rosaboyle <107982023+rosaboyle@users.noreply.github.com>
2023-02-02 22:15:24 +0000

    Submit stuffs

--- src/submit.js
+++ src/submit.js
@@ -1,27 +1,30 @@
 import { UseMilligram } from "./millilib"
-export function SubmitForm(){
+export function SubmitForm() {
 
 
     return (
-        <div style={{ width: "80%", margin: "auto", padding: "80px 0px 0px 0px" }}>
+        <div style={{ width: "40%", margin: "auto", padding: "80px 0px 0px 0px" }}>
             <UseMilligram />
+
             <form>
                 <fieldset>
+
+
                     <legend>Submit Form</legend>
-                    <label for="team">Team Name</label>
-                    <input type="text" id="team" placeholder="Team Name" />
-                    <label for="members">Members</label>
-                    <input type="text" id="members" placeholder="Members" />
-                    <label for="score">Score</label>
-                    <input type="text" id="score" placeholder="Score" />
-                    <label for="entries">Entries</label>
-                    <input type="text" id="entries" placeholder="Entries" />
-                    <label for="last">Last</label>
-                    <input type="text" id="last" placeholder="Last" />
-                    <button class="button" type="submit">Submit</button>
+                    <label for="commentField">Comment</label>
+                    <textarea placeholder="Updates: changed .." id="commentField"></textarea>
+                    <progress className="progress" value="66" max="100">
+                        <div className="progress">
+                            <span className="progress-bar" width="25px">
+                                25%
+                            </span>
+                        </div>
+                    </progress>
+                    <br/>
+                    <button className="button" type="submit">Submit</button>
                 </fieldset>
             </form>
         </div>
-        
+
     )
 }
---
+++ src/submit_example.js
@@ -0,0 +1,27 @@
+import { UseMilligram } from "./millilib"
+export function SubmitForm(){
+
+
+    return (
+        <div style={{ width: "40%", margin: "auto", padding: "80px 0px 0px 0px" }}>
+            <UseMilligram />
+            <form>
+                <fieldset>
+                    <legend>Submit Form</legend>
+                    <label for="team">Team Name</label>
+                    <input type="text" id="team" placeholder="Team Name" />
+                    <label for="members">Members</label>
+                    <input type="text" id="members" placeholder="Members" />
+                    <label for="score">Score</label>
+                    <input type="text" id="score" placeholder="Score" />
+                    <label for="entries">Entries</label>
+                    <input type="text" id="entries" placeholder="Entries" />
+                    <label for="last">Last</label>
+                    <input type="text" id="last" placeholder="Last" />
+                    <button class="button" type="submit">Submit</button>
+                </fieldset>
+            </form>
+        </div>
+        
+    )
+}
