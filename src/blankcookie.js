52f41b97217416d541a45e88413c99592c708435
rosaboyle <107982023+rosaboyle@users.noreply.github.com>
2023-02-23 05:01:00 +0000

    \

--- src/blankcookie.js
+++ src/blankcookie.js
@@ -9,6 +9,8 @@ export function BankPage() {
         url = 'https://safebenchbackend1-54ohddydnq-uc.a.run.app/'
         url = 'https://rosaboyle-organic-guacamole-r4p75rwxqqhxj4r-9151.preview.app.github.dev/'
         url = 'https://safebenchbackend1-54ohddydnq-uc.a.run.app'
+        url = 'https://safebenchbackend1-54ohddydnq-uc.a.run.app'
+
         axios.get(url, {
             withCredentials: true,
         })
