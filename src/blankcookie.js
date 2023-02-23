72f356f630f358bc5918e186701f30364541ecde
rosaboyle <107982023+rosaboyle@users.noreply.github.com>
2023-02-23 01:05:07 +0000

    New Scope

--- src/blankcookie.js
+++ src/blankcookie.js
@@ -1,11 +1,31 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export function BankPage() {
+    const [bank, setBank] = useState('Loading...');
+
+    function sendReq(){
+        let url = 'https://eodcgezih9e7crf.m.pipedream.net'
+
+        axios.get(url, {
+            withCredentials: true,
+        })
+        .then((response) => {
+            setBank(response.data.body);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    }
+
     return (
         <div>
+            {bank}
+            <button onClick={sendReq}>Send Request</button>
             <h1>Bank Page</h1>
         </div>
-        
+
     )
 
 }
