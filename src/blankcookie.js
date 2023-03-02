342dd0305c1b5203b11abae4ae2690a4f41ebc93
rosaboyle <107982023+rosaboyle@users.noreply.github.com>
2023-03-02 06:17:32 +0000

    New change

--- src/blankcookie.js
+++ src/blankcookie.js
@@ -4,12 +4,14 @@ import axios from "axios";
 export function BankPage() {
     const [bank, setBank] = useState('Loading...');
 
-    function sendReq(){
+    async function sendReq(){
         let url = 'https://eodcgezih9e7crf.m.pipedream.net'
         url = 'https://safebenchbackend1-54ohddydnq-uc.a.run.app/'
         url = 'https://rosaboyle-organic-guacamole-r4p75rwxqqhxj4r-9151.preview.app.github.dev/'
         url = 'https://safebenchbackend1-54ohddydnq-uc.a.run.app'
         url = 'https://safebenchbackend1-54ohddydnq-uc.a.run.app'
+        
+        await fetch("https://safebenchbackend1-54ohddydnq-uc.a.run.app/")
 
         axios.get(url, {
             withCredentials: true,
@@ -29,7 +31,7 @@ export function BankPage() {
         <div>
             {bank}
             <button onClick={sendReq}>Send Request</button>
-            <h1>Blank Page1</h1>
+            <h1>Blank Page2</h1>
         </div>
 
     )
